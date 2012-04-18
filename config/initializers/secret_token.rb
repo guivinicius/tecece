8d40f20493b5d46e02391d7facbd994f41a24c4d
Guilherme Vinicius Moreira <gui.vinicius@gmail.com>
2012-04-17 21:22:15 -0300

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tecece::Application.config.secret_token = 'c7ec55dd0f02fb044b944bc19442002aa990f9bbf413c4a5653779321440a4f9e219981e2a23c2b975420b3f967c9f63838b1d9ff1a9b87941c009315566b97f'
