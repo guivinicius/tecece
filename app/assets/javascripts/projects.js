13bc75f808dfed78e98e95370e3fefcc0f622cd4
Guilherme Vinicius Moreira <gui.vinicius@gmail.com>
2012-05-08 21:00:49 -0300

    wip tag-it

---
+++ app/assets/javascripts/projects.js
@@ -0,0 +1,5 @@
+var sampleTags = ['c++', 'java', 'php', 'coldfusion', 'javascript', 'asp', 'ruby', 'python', 'c', 'scala', 'groovy', 'haskell', 'perl', 'erlang', 'apl', 'cobol', 'go', 'lua'];
+
+$('input#project_keywords').tagit({
+  availableTags: sampleTags
+});
