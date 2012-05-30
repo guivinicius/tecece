16244932630e392441fc940d2533bb3179aef18a
Guilherme Vinicius Moreira <gui.vinicius@gmail.com>
2012-05-30 18:56:04 -0300

    Removing home controller and refactoring routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,11 @@
 Tecece::Application.routes.draw do
 
-  get "dashboard/index"
-
   devise_for :users
 
+  resources :dashboard
   resources :projects
   resources :colleges
 
-  authenticated :user do
-    root :to => "dashboard#index"
-  end
-
-  root :to => 'home#index'
+  root :to => 'dashboard#index'
 
 end
