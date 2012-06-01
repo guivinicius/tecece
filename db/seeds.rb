27acf8112e515dbb17100a5b9aa451424f46c465
Guilherme Vinicius Moreira <gui.vinicius@gmail.com>
2012-06-01 16:18:57 -0300

    Added some college seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +6,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+  College.create(:name => "Faculdade de Tecnologia de Sorocaba", :short => "FATEC Sorocaba")
+  College.create(:name => "Universidade de Sorocaba", :short => "UNISO")
+  College.create(:name => "Esacola de Administração, Marketing e Comunicação", :short => "ESAMC")
+  College.create(:name => "Faculdade de Engenharia de Sorocaba", :short => "FACENS")
+  College.create(:name => "Universidade Paulista Sorocaba", :short => "UNIP Sorocaba")
+  College.create(:name => "Faculdade de Direito de Sorocaba", :short => "FADI Sorocaba")
+  College.create(:name => "Universidade Federal de São Carlos - Sorocaba", :short => "UFSCAR Sorocaba")
+  College.create(:name => "Acadêmia de Ensino Superior - Sorocaba", :short => "AES Sorocaba")
